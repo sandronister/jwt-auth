425ad12714e890f99cd01ac12c0a7caeec998b11
Julio Sandroni <sandronister@gmail.com>
2024-03-09 17:34:13 -0300

    fix:return usecase dto

--- internal/handlers/userhandlers/current.go
+++ internal/handlers/userhandlers/current.go
@@ -6,16 +6,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type UserDTO struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+}
+
 func (h *Handler) Current(c *gin.Context) {
 
 	id, _ := c.Get("user_id")
 
 	user, err := h.userUsecase.GetUserById(id.(int))
 
+	userDTO := UserDTO{
+		ID:       user.ID,
+		Username: user.Username,
+	}
+
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"user": user})
+	c.JSON(http.StatusOK, gin.H{"user": userDTO})
 }
